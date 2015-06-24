fbaa0e49199bf1c8e9b9f956f3c7df5dd5632874
imalbert <albert.s.manuel@gmail.com>
2015-06-24 11:44:08 +0800

    Added a posts collection

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -2,17 +2,20 @@ var postsData = [
 	{
 		title: 'Introducing Telescope',
 		author: 'Sacha Greif',
-		url: 'http://sachagreif.com/introducing-telescope/'
+		url: 'http://sachagreif.com/introducing-telescope/',
+		img: 'Chrysanthemum.jpg'
 	},
 	{
 		title: 'Meteor',
 		author: 'Tom Coleman',
-		url: 'http://meteor.com'
+		url: 'http://meteor.com',
+		img: 'Desert.jpg'
 	},
 	{
 		title: 'The Meteor Book',
 		author: 'Tom Coleman',
-		url: 'http://themeteorbook.com'
+		url: 'http://themeteorbook.com',
+		img: 'Koala.jpg'
 	}
 ];
 Template.postList.helpers({
