abc9f6f5fef11b3cec5ae24771967d00599b3446
imalbert <albert.s.manuel@gmail.com>
2015-06-30 18:33:05 +0800

    commit7-5

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -6,9 +6,16 @@ Template.postSubmit.events({
 			url: $(e.target).find('[name=url]').val(),
 			title: $(e.target).find('[name=title]').val(),
 			message: $(e.target).find('[name=message]').val()
-		}
+		};
 
-		post._id = Posts.insert(post);
-		Router.go('postPage', post);
+		// post._id = Posts.insert(post);
+		// Router.go('postPage', post);
+
+		// Meteor.call('post', post, function(error, id)) {
+		// 	if (error)
+		// 		return alert(error.reason);
+
+		// 	Router.go('postPage', {_id: id});
+		// });
 	}
 })
